20590214798034c51289ddc06d04a1ac9fb08be1
jorge ramos <55595863+jorgera10@users.noreply.github.com>
2021-11-23 17:24:56 -0500

    parte5 js

---
+++ Intforlan_Store/js/alertas.js
@@ -0,0 +1,64 @@
+function alerta(){
+    swal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Producto añadido correctamente',
+        showConfirmButton: false,
+        timer: 4000,
+        // title:
+        // text:
+        // html:
+        // confirmButtonText:
+        // footer:
+        // width:
+        // padding:
+        // background:
+        // grow:
+        // toast:
+        // allowOutsideClick:
+        // allowEscapeKey:
+        // allowEnterKey:
+        // stopKeydownPropagation:
+    
+        // input:
+        // inputPlaceholder:
+        // inputValue:
+        // inputOptions:
+        
+        //  customClass:
+        // 	container:
+        // 	popup:
+        // 	header:
+        // 	title:
+        // 	closeButton:
+        // 	icon:
+        // 	image:
+        // 	content:
+        // 	input:
+        // 	actions:
+        // 	confirmButton:
+        // 	cancelButton:
+        // 	footer:	
+    
+        // showConfirmButton:
+        // confirmButtonColor:
+        // confirmButtonAriaLabel:
+    
+        // showCancelButton:
+        // cancelButtonText:
+        // cancelButtonColor:
+        // cancelButtonAriaLabel:
+        
+        // buttonsStyling:
+        // showCloseButton:
+        // closeButtonAriaLabel:
+    
+    
+        // imageUrl:
+        // imageWidth:
+        // imageHeight:
+        // imageAlt:
+    })
+}
+
+document.getElementById( "Agregar" ).addEventListener( "click" ,alerta ())
